f65545eac6c3ee796cfc7b9fe08ec774c706df77
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-01-25 09:35:57 +0530

    Create Fair Distribution of Cookies.java

---
+++ BACKTRACKING/Fair Distribution of Cookies.java
@@ -0,0 +1,25 @@
+//leetcode => https://leetcode.com/problems/fair-distribution-of-cookies/description/
+
+class Solution {
+    int res = Integer.MAX_VALUE;
+    public int distributeCookies(int[] cookies, int k) {
+        dfs(cookies, 0, k, new int[k]);
+        return res;
+    }
+
+    void dfs(int[] cookies, int cur, int k, int[] children) {
+        if (cur == cookies.length) {
+            int max = 0;
+            for (int c : children) max = Math.max(max, c);
+                res = Math.min(res, max);
+                return;
+        }
+            for (int i = 0; i < k; i++) {
+                children[i] += cookies[cur];
+                dfs(cookies, cur + 1, k, children);
+                children[i] -= cookies[cur];
+        }
+}
+}
+//time: O(k^n) where n is cookies.length
+//space: O(k+n) where k is children array, n is number of call stacks
