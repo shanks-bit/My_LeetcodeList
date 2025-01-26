2effa9bf63dbe522258ea4bd6323ae8a21148f53
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-01-26 08:43:23 +0530

    Create Combination Sum IV.java

---
+++ BACKTRACKING/Combination Sum IV.java
@@ -0,0 +1,31 @@
+//leetcode => https://leetcode.com/problems/combination-sum-iv/
+
+class Solution {
+    int[] t;
+    
+    public int combinationSum4(int[] nums, int target) {
+        t = new int[target+1];
+        Arrays.fill(t,-1);
+        return solve(nums,target);
+    }
+
+    public int solve(int[] nums, int target)
+    {
+        if(target == 0) {
+            return 1;
+        }
+        if(target < 0) {
+            return 0;
+        }
+        
+        if(t[target] != -1) {
+            return t[target];
+        }
+        
+        int result = 0;
+        for(int i = 0; i<nums.length;i++) {
+            result += solve(nums, target-nums[i]);
+        }
+        return t[target] = result;
+    }
+}
