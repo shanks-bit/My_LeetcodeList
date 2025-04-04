9d68474b2d2a6a08761d6ee094e48d857e0eed4c
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-04-04 08:07:44 +0530

    Create Maximum Absolute Sum of Any Subarray.java

---
+++ CUMULATIVE_SUM(PREFIX_ARRAY)/Maximum Absolute Sum of Any Subarray.java
@@ -0,0 +1,29 @@
+//leetcode => https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/description/
+//KDANES ALGORITHM
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int n = nums.length;
+
+        int maxSum = nums[0];
+        int minSum = nums[0];
+
+        int currSumMax = nums[0];
+        int currSumMin = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            //Kadane's Algo for finding max subarray sum
+            currSumMax = Math.max(nums[i], currSumMax + nums[i]);
+            maxSum = Math.max(maxSum, currSumMax);
+
+            //Kadane's Algo for finding min subarray sum
+            currSumMin = Math.min(nums[i], currSumMin + nums[i]);
+            minSum = Math.min(minSum, currSumMin);
+        }
+
+        return Math.max(maxSum, Math.abs(minSum));
+    
+    }
+}
+//Approach-1 (1 Pass - Kadane's Algorithm for maxsubarrysum and minsybarraysum inside one for loop)
+//T.C : O(n)
+//S.C : O(1)
