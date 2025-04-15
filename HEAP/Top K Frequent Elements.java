d4996be25a992e8bc89badaf0377e9c1591aeda3
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-04-15 12:37:22 +0530

    Create Top K Frequent Elements.java

---
+++ HEAP/Top K Frequent Elements.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/top-k-frequent-elements/description/
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+             map.put(num, map.getOrDefault(num, 0) + 1); 
+        }
+        
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        for(int key : map.keySet()) {
+            heap.add(key); 
+        }
+        
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < k; i++) {
+            ans.add(heap.poll());
+        }
+        
+        return ans;
+    }
+}
