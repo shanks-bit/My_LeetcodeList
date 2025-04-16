7033415c225de020c7e875cb9b712d0575e58347
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-04-16 08:15:37 +0530

    Update Top K Frequent Elements.java

--- HEAP/Top K Frequent Elements.java
+++ HEAP/Top K Frequent Elements.java
@@ -20,3 +20,33 @@ class Solution {
         return ans;
     }
 }
+
+//bucket sort
+//https://leetcode.com/problems/top-k-frequent-elements/solutions/5032156/beats-96-39-of-users-with-java-simple-well-explained-bucket-sorting-hashmap-solution
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num,0) + 1);
+        }
+        for (int key : hm.keySet()) {
+            int freq = hm.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+        int[] ans = new int[k];
+        int pos = 0;
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (int j = 0; j < bucket[i].size() && pos < k; j++) {
+                    ans[pos] = bucket[i].get(j);
+                    pos++;
+                }
+            }
+        }
+        return ans;
+    }
+}
