43336a770464ad9eeb9731b8168982803ab3efdc
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-05-09 08:53:30 +0530

    Update Topological Sort.java

--- GRAPH/BFS-DFS/Topological Sort.java
+++ GRAPH/BFS-DFS/Topological Sort.java
@@ -38,3 +38,45 @@ class Solution {
         return result;
     }
 }
+
+// BFS
+// KAHN'S ALGORITHM
+import java.util.*;
+class Solution {
+    // Function to return list containing vertices in Topological order
+    public ArrayList<Integer> topoSort(int N, ArrayList<ArrayList<Integer>> adj) {
+        Queue<Integer> que = new LinkedList<>();
+        int[] indegree = new int[N];
+        
+        // 1. Calculate indegrees
+        for (int u = 0; u < N; u++) {
+            for (int v : adj.get(u)) {
+                indegree[v]++;
+            }
+        }
+        
+        // 2. Fill queue with nodes having indegree 0
+        for (int i = 0; i < N; i++) {
+            if (indegree[i] == 0) {
+                que.add(i);
+            }
+        }
+        
+        // 3. Simple BFS
+        ArrayList<Integer> result = new ArrayList<>();
+        while (!que.isEmpty()) {
+            int u = que.poll();
+            result.add(u);
+            
+            for (int v : adj.get(u)) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    que.add(v);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
