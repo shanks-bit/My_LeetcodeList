051c39e60a82465867780fad050e0613e3f3f184
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-06-16 07:05:10 +0530

    Update Top K Frequent Elements.java

--- HEAP/Top K Frequent Elements.java
+++ HEAP/Top K Frequent Elements.java
@@ -6,7 +6,8 @@ class Solution {
         for (int num : nums) {
              map.put(num, map.getOrDefault(num, 0) + 1); 
         }
-        
+
+        //heap 'the less frequent element first'
         Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
         for(int key : map.keySet()) {
             heap.add(key); 
