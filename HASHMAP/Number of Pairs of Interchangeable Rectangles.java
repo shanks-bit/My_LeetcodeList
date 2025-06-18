42e74c376c4616030f35c2d5af1aad80a36ec540
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-06-18 08:41:30 +0530

    Create Number of Pairs of Interchangeable Rectangles.java

---
+++ HASHMAP/Number of Pairs of Interchangeable Rectangles.java
@@ -0,0 +1,20 @@
+// leetcode -> https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/description/
+class Solution {
+    public long interchangeableRectangles(int[][] rectangles) {
+        HashMap<Double, Long> map = new HashMap<>();
+        long count = 0;
+		
+        for (int[] A : rectangles) {
+		
+            double x = A[0];
+            double y = A[1];
+            double division = (double) x / y;
+			
+            if (map.containsKey(division)) {
+                count += map.get(division);
+            }
+            map.put(division, map.getOrDefault(division, 0L) + 1);
+        }
+        return count;
+    }
+}
