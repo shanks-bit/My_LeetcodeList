1f7abf632b06c4726d36a67d2e219946275bb76a
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-07-11 08:08:58 +0530

    Update Non-decreasing Array.java

--- GREEDY/Non-decreasing Array.java
+++ GREEDY/Non-decreasing Array.java
@@ -1,8 +1,8 @@
 // leetcode -> https://leetcode.com/problems/non-decreasing-array/description/
 /*
-This problem is like a greedy problem. When you find nums[i-1] > nums[i] for some i, you will prefer to change nums[i-1]'s value, since a larger nums[i] will give you more risks
-that you get inversion errors after position i. But, if you also find nums[i-2] > nums[i], then you have to change nums[i]'s value instead, or else you need to change both
-of nums[i-2]'s and nums[i-1]'s values.
+This problem is like a greedy problem. When you find nums[i-1] > nums[i] for some i, you will prefer to change nums[i-1]'s value, since a larger nums[i]
+will give you more risks that you get inversion errors after position i. But, if you also find nums[i-2] > nums[i], then you have to change nums[i]'s value
+instead, or else you need to change both of nums[i-2]'s and nums[i-1]'s values.
 */
 class Solution {
     public boolean checkPossibility(int[] nums) {
