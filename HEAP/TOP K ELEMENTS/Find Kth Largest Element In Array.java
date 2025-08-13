d48db44ef0c216f162359afe74e4b04b8ab91f45
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-08-13 23:52:06 +0530

    Update Find Kth Largest Element In Array.java

--- HEAP/TOP K ELEMENTS/Find Kth Largest Element In Array.java
+++ HEAP/TOP K ELEMENTS/Find Kth Largest Element In Array.java
@@ -1,1 +1,17 @@
+//leetcode => https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heapMin = new PriorityQueue<>();
+        for (int i=0; i<k; i++) {
+            heapMin.offer(nums[i]);
+        }
 
+        for (int i=k; i<nums.length; i++) {
+            if (nums[i] > heapMin.peek()) {
+                heapMin.poll();
+                heapMin.offer(nums[i]);
+            }
+        }
+        return heapMin.peek();
+    }
+}
