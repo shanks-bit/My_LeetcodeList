6d3de792e9e079e44464c38011a6d3b8e08dd1ad
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-08-30 13:48:25 +0530

    Update Non-decreasing Subsequences.java

--- BACKTRACKING/Non-decreasing Subsequences.java
+++ BACKTRACKING/Non-decreasing Subsequences.java
@@ -1,31 +1,33 @@
 //leetcode => https://leetcode.com/problems/non-decreasing-subsequences/
 
 class Solution {
-    private int n;
-    public List<List<Integer>> findSubsequences(int[] nums) {
-        n = nums.length;
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-
-        backtrack(nums, 0, curr, result);
-
-        return result;
+    private void backtrack(int[] nums, int index, List<Integer> sequence,
+            Set<List<Integer>> result) {
+        // if we have checked all elements
+        if (index == nums.length) {
+            if (sequence.size() >= 2) {
+                result.add(new ArrayList<>(sequence));
+            }
+            return;
+        }
+        // if the sequence remains increasing after appending nums[index]
+        if (sequence.isEmpty() ||
+                sequence.get(sequence.size() - 1) <= nums[index]) {
+            // append nums[index] to the sequence
+            sequence.add(nums[index]);
+            // call recursively
+            backtrack(nums, index + 1, sequence, result);
+            // delete nums[index] from the end of the sequence
+            sequence.remove(sequence.size() - 1);
+        }
+        // call recursively not appending an element
+        backtrack(nums, index + 1, sequence, result);
     }
 
-    private void backtrack(int[] nums, int idx, List<Integer> curr, List<List<Integer>> result) {
-        if (curr.size() > 1) {
-            result.add(new ArrayList<>(curr));
-        }
-
-        Set<Integer> st = new HashSet<>();
-        for (int i = idx; i < n; i++) {
-            if ((curr.isEmpty() || nums[i] >= curr.get(curr.size() - 1)) && !st.contains(nums[i])) {
-                curr.add(nums[i]);  //Add element
-                backtrack(nums, i + 1, curr, result);  //Explore element
-                curr.remove(curr.size() - 1);  //Remove element
-
-                st.add(nums[i]);
-            }
-        }
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> result = new HashSet<List<Integer>>();
+        List<Integer> sequence = new ArrayList<Integer>();
+        backtrack(nums, 0, sequence, result);
+        return new ArrayList(result);
     }
 }
