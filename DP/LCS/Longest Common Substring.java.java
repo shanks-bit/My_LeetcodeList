e2a9e7181aa96e9d3f30e1335f63f68616ed9e3f
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-12-11 13:18:47 +0530

    Create Longest Common Substring.java.java

---
+++ DP/LCS/Longest Common Substring.java.java
@@ -0,0 +1,17 @@
+// leetcode -> https://www.geeksforgeeks.org/dsa/longest-common-substring-dp-29/
+
+int countCommon(string a, string b, int m, int n) {
+	int t[m + 1][n + 1];
+	int best_count = 0;
+
+	for (int i = 1; i < m + 1; i++)
+		for (int j = 1; j < n + 1; j++)
+			if (a[i - 1] == b[j - 1]) {
+				t[i][j] = 1 + t[i - 1][j - 1];
+				best_count = max(t[i][j], best_count);
+			}
+			else
+				t[i][j] = 0;
+
+	return best_count;
+}
