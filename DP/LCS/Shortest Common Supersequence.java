c5bcf68a4561d5c87a824683ae093008e6930119
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-12-13 07:44:38 +0530

    Create Shortest Common Supersequence.java

---
+++ DP/LCS/Shortest Common Supersequence.java
@@ -0,0 +1,7 @@
+// gfg -> https://www.geeksforgeeks.org/dsa/shortest-common-supersequence/
+
+/* find longest common subsequence as lcs then
+  shortest common subsequence = m + n - lcs
+  m = length of 1st dtring
+  n = length of 2nd string
+*/
