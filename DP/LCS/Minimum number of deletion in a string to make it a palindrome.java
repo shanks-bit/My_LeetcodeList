69a7687519b0117acd875c2d4ec6cf1e81e8136c
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-12-13 13:42:42 +0530

    Create Minimum number of deletion in a string to make it a palindrome.java

---
+++ DP/LCS/Minimum number of deletion in a string to make it a palindrome.java
@@ -0,0 +1,8 @@
+// gfg -> https://www.geeksforgeeks.org/dsa/minimum-number-deletions-make-string-palindrome/
+
+/* length of longest palindromic subsequence = 1 / No. of deletions
+
+  so Min. no. of deletions = length of string - lps
+  and Min. no. of insertions = length of string - lps
+*/
+  
