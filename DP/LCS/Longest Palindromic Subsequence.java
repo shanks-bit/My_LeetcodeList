db7d5caa2bbcd5183dfd7d3d162735c0b386a107
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-12-13 12:39:31 +0530

    Implement Longest Palindromic Subsequence explanation
    Added explanation for the longest palindromic subsequence method.

---
+++ DP/LCS/Longest Palindromic Subsequence.java
@@ -0,0 +1,7 @@
+// leetcode -> https://leetcode.com/problems/longest-palindromic-subsequence/description/
+
+/* above method will work in this way---
+  a = "apple" , b = "elppa"       (b string is reverse of a string)
+  longest palindromic subsequence = longest common subsequence(a, b)  //how it is done in lcs
+
+*/
