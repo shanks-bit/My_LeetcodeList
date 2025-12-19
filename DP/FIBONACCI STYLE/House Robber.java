e3f1b1eb98d3e989b11cfcc42de75cef7c3df22d
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-12-19 23:00:51 +0530

    Update House Robber.java

--- DP/FIBONACCI STYLE/House Robber.java
+++ DP/FIBONACCI STYLE/House Robber.java
@@ -1,1 +1,40 @@
+// leetcode => https://leetcode.com/problems/house-robber/description/
+// bottom up approach
+class Solution {
+    public int rob(int[] nums) {
+        //bottom up approach
+        if (nums.length == 1) {
+            return nums[0];class Solution {
+    private HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
+    private int[] nums;
+    
+    private int dp(int i) {
+        // Base cases
+        if (i == 0) return nums[0];
+        if (i == 1) return Math.max(nums[0], nums[1]);
+        if (!memo.containsKey(i)) {
+            memo.put(i, Math.max(dp(i - 1), dp(i - 2) + nums[i])); // Recurrence relation
+        }
+        return memo.get(i);
+    }
+    
+    public int rob(int[] nums) {
+        this.nums = nums;
+        return dp(nums.length - 1);
+    }
+}
+        }
+        int[] dp = new int[nums.length];
+        // Base cases
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]); // Recurrence relation
+        }
+        
+        return dp[nums.length - 1];
+    }
+}
 
+// top down approach
