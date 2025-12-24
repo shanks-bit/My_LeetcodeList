17b55951ad27114277baf40fe91f36a71572342c
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2025-12-24 12:24:54 +0530

    Update Koko Eating Bananas.java

--- BINARY_SEARCH/On Answer/Condition Function/Koko Eating Bananas.java
+++ BINARY_SEARCH/On Answer/Condition Function/Koko Eating Bananas.java
@@ -1,1 +1,32 @@
+//leetcode => https://leetcode.com/problems/koko-eating-bananas/description/
 
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = (int) Arrays.stream(piles).max().getAsInt();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (canEatAll(piles, mid, h)) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean canEatAll(int[] piles, int givenHours, int h) {
+        int actualHours = 0;
+        for (int x : piles) {
+            actualHours += x / givenHours;
+
+            if (x % givenHours != 0) {
+                actualHours++;
+            }        
+        }
+        return actualHours <= h;
+    }
+}
