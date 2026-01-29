f290ef93ca55ea71628af43a65e514e47f9f18a2
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-01-29 19:15:35 +0530

    Create Decode ways.java

---
+++ DP/FIBONACCI STYLE/Decode ways.java
@@ -0,0 +1,70 @@
+//  https://leetcode.com/problems/decode-ways/description/
+
+//top down
+class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int numDecodings(String s) {
+        return recursiveWithMemo(0, s);
+    }
+
+    private int recursiveWithMemo(int index, String str) {
+        // Have we already seen this substring?
+        if (memo.containsKey(index)) {
+            return memo.get(index);
+        }
+
+        // If you reach the end of the string
+        // Return 1 for success.
+        if (index == str.length()) {
+            return 1;
+        }
+
+        // If the string starts with a zero, it can't be decoded
+        if (str.charAt(index) == '0') {
+            return 0;
+        }
+
+        if (index == str.length() - 1) {
+            return 1;
+        }
+
+        int ans = recursiveWithMemo(index + 1, str);
+        if (Integer.parseInt(str.substring(index, index + 2)) <= 26) {
+            ans += recursiveWithMemo(index + 2, str);
+        }
+
+        // Save for memoization
+        memo.put(index, ans);
+
+        return ans;
+    }
+}
+
+// bottom up
+class Solution {
+    public int numDecodings(String s) {
+        // DP array to store the subproblem results
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+
+        // Ways to decode a string of size 1 is 1. Unless the string is '0'.
+        // '0' doesn't have a single digit decode.
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i < dp.length; i++) {
+            // Check if successful single digit decode is possible.
+            if (s.charAt(i - 1) != '0') {
+                dp[i] = dp[i - 1];
+            }
+
+            // Check if successful two digit decode is possible.
+            int twoDigit = Integer.valueOf(s.substring(i - 2, i));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
