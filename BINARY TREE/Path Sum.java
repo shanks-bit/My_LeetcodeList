1c770181f2c136ba385f7fd7d7c501dc57012e8b
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-02-06 11:51:35 +0530

    Refactor path sum logic to iterative approach
    Refactor hasPathSum method to use iteration and simplify logic.

--- BINARY TREE/Path Sum.java
+++ BINARY TREE/Path Sum.java
@@ -1,61 +1,44 @@
-//top-down
-//leetcode => https://leetcode.com/problems/path-sum/description/
+//https://leetcode.com/problems/path-sum/editorial/
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// recursion
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        return pathSum(root, targetSum, 0);
-    }
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
 
-    private boolean pathSum(TreeNode root, int sum, int curr) {
-        if (root == null) {
-            return false;
-        }
-
-        if (root.left == null && root.right == null) {
-            return (curr + root.val) == sum; //true or false
-        }
-
-        boolean l = pathSum(root.left, sum, curr + root.val);
-        boolean r = pathSum(root.right, sum, curr + root.val);
-
-        return l || r; //true or false if this condition happppens true will be the result
+        sum -= root.val;
+        if ((root.left == null) && (root.right == null)) return (sum == 0);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 }
-/*
-Now, l || r means:
 
-    l is checking: Is there a valid path in the left subtree? (true or false)
+// iteration
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
 
-    r is checking: Is there a valid path in the right subtree? (true or false)
+        LinkedList<TreeNode> node_stack = new LinkedList();
+        LinkedList<Integer> sum_stack = new LinkedList();
+        node_stack.add(root);
+        sum_stack.add(sum - root.val);
 
-Finally, l || r returns:
+        TreeNode node;
+        int curr_sum;
+        while (!node_stack.isEmpty()) {
+            node = node_stack.pollLast();
+            curr_sum = sum_stack.pollLast();
+            if (
+                (node.right == null) && (node.left == null) && (curr_sum == 0)
+            ) return true;
 
-    true if either l is true or r is true.
-
-    false if both l and r are false.
-
-Why do we use return l || r?
-
-Because you want to find at least one path from root to any leaf where the sum of node values equals the target sum.
-
-    If any one path (left or right) satisfies the condition (curr + root.val == sum at the leaf), you want to return true.
-
-    Otherwise, return false.
-
-That's why you combine the left and right recursive results using || (logical OR).
-*/
+            if (node.right != null) {
+                node_stack.add(node.right);
+                sum_stack.add(curr_sum - node.right.val);
+            }
+            if (node.left != null) {
+                node_stack.add(node.left);
+                sum_stack.add(curr_sum - node.left.val);
+            }
+        }
+        return false;
+    }
+}
