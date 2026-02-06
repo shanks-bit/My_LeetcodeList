55f5f7455a1c66cbfcf7d4a67266b0c7601718cf
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-02-06 12:15:39 +0530

    Create Range Sum of BST.java

---
+++ BINARY TREE/Range Sum of BST.java
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/range-sum-of-bst/editorial/
+
+// recursion
+class Solution {
+    int ans;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        ans = 0;
+        dfs(root, low, high);
+        return ans;
+    }
+
+    public void dfs(TreeNode node, int low, int high) {
+        if (node != null) {
+            if (low <= node.val && node.val <= high)
+                ans += node.val;
+            if (low < node.val)
+                dfs(node.left, low, high);
+            if (node.val < high)
+                dfs(node.right, low, high);
+        }
+    }
+}
+
+// iteration
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int ans = 0;
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                if (low <= node.val && node.val <= high)
+                    ans += node.val;
+                if (low < node.val)
+                    stack.push(node.left);
+                if (node.val < high)
+                    stack.push(node.right);
+            }
+        }
+        return ans;
+    }
+}
