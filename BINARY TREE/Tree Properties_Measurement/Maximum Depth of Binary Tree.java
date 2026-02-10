398bdd811b05fe6071288c8412aac598722e40dc
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-02-10 13:13:53 +0530

    Create Maximum Depth of Binary Tree.java

---
+++ BINARY TREE/Tree Properties_Measurement/Maximum Depth of Binary Tree.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/editorial/
+
+// dfs
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return java.lang.Math.max(left_height, right_height) + 1;
+        }
+    }
+}
+
+// bfs
+class Solution {
+    public int maxDepth(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> depths = new LinkedList<>();
+        if (root == null) return 0;
+
+        stack.add(root);
+        depths.add(1);
+
+        int depth = 0, current_depth = 0;
+        while (!stack.isEmpty()) {
+            root = stack.pollLast();
+            current_depth = depths.pollLast();
+            if (root != null) {
+                depth = Math.max(depth, current_depth);
+                stack.add(root.left);
+                stack.add(root.right);
+                depths.add(current_depth + 1);
+                depths.add(current_depth + 1);
+            }
+        }
+        return depth;
+    }
+}
