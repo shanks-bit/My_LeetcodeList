dfb69923a621b219405d4efc5bd069a2b840c0d8
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-03-15 22:08:24 +0530

    Update Invert Binary Tree.java

--- BINARY TREE/Tree Construction & Modification/Invert Binary Tree.java
+++ BINARY TREE/Tree Construction & Modification/Invert Binary Tree.java
@@ -1,1 +1,33 @@
 // https://leetcode.com/problems/invert-binary-tree/description/
+
+//iteration
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
+        return root;
+    }
+}
+
+//recursion
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
