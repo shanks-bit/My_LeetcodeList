218edde8807c18582df5f86d6e337a1d9460e749
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-04-06 09:13:15 +0530

    Update Find X-Sum of All K-Long Subarrays I.java

--- HASHMAP/Find X-Sum of All K-Long Subarrays I.java
+++ HASHMAP/Find X-Sum of All K-Long Subarrays I.java
@@ -1,45 +1,30 @@
 //leetcode -> https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i/description/
+
 class Solution {
+
     public int[] findXSum(int[] nums, int k, int x) {
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
         int n = nums.length;
-        int left = 0, right = 0, max1 = 0, max2 = 0;
-        int[] ans = new int[Math.max(0, n - k + 1)];
+        int[] ans = new int[n - k + 1];
 
-        for (right = 0; right < k; right++) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-        }
-        ans[0] = computeXSum(map, x);
+        for (int i = 0; i <= n - k; ++i) {
+            Map<Integer, Integer> cnt = new HashMap<>();
+            for (int j = i; j < i + k; ++j) {
+                cnt.put(nums[j], cnt.getOrDefault(nums[j], 0) + 1);
+            }
 
-        for (right = k; right < n; right++) {
-            int add = nums[right];
-            int rem = nums[right - k];
+            List<int[]> freq = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+                freq.add(new int[] { entry.getValue(), entry.getKey() });
+            }
 
-            map.put(add, map.getOrDefault(add, 0) + 1);
-            int fr = map.get(rem) - 1;
-            if (fr == 0) map.remove(rem);
-            else map.put(rem, fr);
-
-            ans[right - k + 1] = computeXSum(map, x);
+            freq.sort((a, b) -> b[0] != a[0] ? b[0] - a[0] : b[1] - a[1]);
+            int xsum = 0;
+            for (int j = 0; j < x && j < freq.size(); ++j) {
+                xsum += freq.get(j)[0] * freq.get(j)[1];
+            }
+            ans[i] = xsum;
         }
 
         return ans;
-    }
-
-    private int computeXSum(Map<Integer, Integer> freq, int x) {
-        List<int[]> items = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
-            items.add(new int[]{e.getKey(), e.getValue()});
-        }
-        items.sort((a, b) -> {
-            if (a[1] != b[1]) return b[1] - a[1];
-            return b[0] - a[0];
-        });
-        long sum = 0;
-        int take = Math.min(x, items.size());
-        for (int i = 0; i < take; i++) {
-            sum += 1L * items.get(i)[0] * items.get(i)[1];
-        }
-        return (int) sum;
     }
 }
