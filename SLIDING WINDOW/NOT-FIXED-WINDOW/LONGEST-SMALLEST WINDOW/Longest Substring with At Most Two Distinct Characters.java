5bebc531898b8d0f29e8f80a4bdff1a48e8513be
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-04-08 00:09:43 +0530

    Update Longest Substring with At Most Two Distinct Characters.java

--- SLIDING WINDOW/NOT-FIXED-WINDOW/LONGEST-SMALLEST WINDOW/Longest Substring with At Most Two Distinct Characters.java
+++ SLIDING WINDOW/NOT-FIXED-WINDOW/LONGEST-SMALLEST WINDOW/Longest Substring with At Most Two Distinct Characters.java
@@ -1,1 +1,21 @@
 // https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/description/
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right=0; right<s.length(); right++) {
+            char curr = s.charAt(right);
+
+            while (charSet.contains(curr)) {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+
+            charSet.add(curr);
+            maxLen = Math.max(maxLen, right-left+1);
+        }
+        return maxLen;
+    }
+}
