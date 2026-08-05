2731c269cef17b4cfae265d6b0d145d2f8da79ef
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-08-05 08:39:38 +0530

    Update Count Number of Ways to Place Houses.java

--- DP/FIBONACCI STYLE/Count Number of Ways to Place Houses.java
+++ DP/FIBONACCI STYLE/Count Number of Ways to Place Houses.java
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/count-number-of-ways-to-place-houses/description/
 
 // top - down approach -> here we are finding solution for one side of road as it would be same for both sides so at last 
-// we are multiplying ans to get for both sides and on one side it will act as a fibbonocci seroes.
+// we are multiplying ans to get for both sides and on one side it will act as a fibbonocci seriess and 
+// whenever no 2 adjacent elements can be choosen uses.
 class Solution {
     long m = 1000000007;
     long[] dp;
