8cebfd0e96fb5055857ce4bb28aab3a4897d0cc1
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-09-02 08:27:36 +0530

    Create Jump Game 2.java

---
+++ GREEDY/Jump game reachiblity-mechanization/Jump Game 2.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/jump-game-ii/description/
+
+class Solution {
+    public int jump(int[] nums) {
+        int near = 0, far = 0, jumps = 0; // near,far and farthest are indexes
+        while (far < nums.length - 1) {
+            int farthest = 0;
+            for (int i = near; i<=far; i++) {
+                farthest = Math.max(farthest, i+nums[i]);                
+            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+}
