fe316312443d0a8f0c1eae50b557d68df9ee744e
Shashank Dhanai <56055059+shanks-bit@users.noreply.github.com>
2026-09-11 07:50:09 +0530

    Create  Separate Black and White Balls.java

---
+++ TWO-POINTER/Separate Black and White Balls.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/separate-black-and-white-balls/description/
+
+class Solution {
+    public long minimumSteps(String s) {
+        long ans = 0;
+        int blackCount = 0; // Tracks the number of black balls (1s)
+
+        // Traverse through the string
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                // White ball encountered, add the number of black balls on its left
+                ans += blackCount;
+            } else {
+                // Black ball encountered, increment the black ball count
+                blackCount++;
+            }
+        }
+        return ans;
+    }
+}
